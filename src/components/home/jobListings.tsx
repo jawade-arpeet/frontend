39a3b3710b102d2b7d287d4e3ce81f93876ca175
jawade-arpeet <jawade.arpeet@gmail.com>
2024-07-03 21:40:54 +0530

    updated ui of right-sidebar.tsx

--- src/components/home/jobListings.tsx
+++ src/components/home/jobListings.tsx
@@ -2,7 +2,7 @@ import { JobCard } from "./jobCard";
 
 export function JobListings() {
   return (
-    <section className="col-span-1 lg:col-span-3 mt-2">
+    <section className="col-span-2 lg:col-span-3 mt-2">
       <h1 className="text-xl font-semibold">Jobs</h1>
       <JobCard jobTitle="" employmentType="" location="" companyName="" />
     </section>
--- src/components/home/right-sidebar.tsx
+++ src/components/home/right-sidebar.tsx
@@ -4,14 +4,16 @@ import { Input } from "../ui/input";
 export function RightSidebar() {
   return (
     <aside className="hidden lg:block lg:col-span-1">
-      <div>
-        <h1>Subsribe</h1>
-        <p>
+      <div className="border border-gray-400 rounded-md p-4">
+        <h1 className="font-semibold text-center">Subscribe</h1>
+        <p className="text-xs font-medium text-gray-400 leading-tight">
           Get quick and personalized job alerts by subscribing to our alerts
         </p>
-        <form>
+        <form className="space-y-1 mt-2">
           <Input placeholder="Enter your email" type="email" name="email" />
-          <Button type="submit">Subscribe</Button>
+          <Button className="w-full" type="submit">
+            Subscribe
+          </Button>
         </form>
       </div>
     </aside>
